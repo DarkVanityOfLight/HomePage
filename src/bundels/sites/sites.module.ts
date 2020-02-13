0d45ce8efbe57a3cd02dfcde9058cd13cd92f12b
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-02-13 08:07:04 +0100

    Quote component

--- src/bundels/sites/sites.module.ts
+++ src/bundels/sites/sites.module.ts
@@ -16,6 +16,7 @@ import { QuoteComponent } from './start/quote/quote.component';
     StartComponent,
     ContactMeComponent,
     AboutMeComponent,
+    QuoteComponent
   ]
 })
 export class SitesModule { }
--- src/bundels/sites/start/quote/quote.component.ts
+++ src/bundels/sites/start/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quote',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.styl']
 })
 export class QuoteComponent implements OnInit {
+  @Input() bgPath: string;
+  @Input() quote: string;
+  imgProperties: number[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.imgProperties[3] = document.documentElement.clientWidth;
+    this.imgProperties[4] = document.documentElement.clientHeight;
+    alert(this.imgProperties[3] + " " +this.imgProperties[4])
+  }
+
+
+
+
+  getImgProperties(): void{
+    const img = new Image();
+    img.onload = () => {
+      this.imgProperties[0] = img.width;
+      this.imgProperties[1] = img.height;
+    }
+    img.src = this.bgPath;
   }
 
 }
