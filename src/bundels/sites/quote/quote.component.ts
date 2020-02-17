1a679aaac7d56495212ecd514fb2181aef0849ee
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-02-17 17:55:49 +0100

    Finsihed quote

--- src/bundels/sites/quote/quote.component.ts
+++ src/bundels/sites/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quote',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.styl']
 })
 export class QuoteComponent implements OnInit {
+  @Input() bgPath: string;
+  @Input() quote: string;
+  imgWidth: number;
+  imgHeight: number;
+  userWidth: number;
+  userHeight: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getImageProperties();
+    this.getUserProperties();
+    
+  }
+
+  getImageProperties(){
+    var img = new Image();
+    img.onload = () =>{
+      this.imgHeight = img.height;
+      this.imgWidth = img.width;
+      
+    }
+    img.src = this.bgPath;
+  }
+
+  getUserProperties(){
+    this.userWidth = window.screen.width;
+    this.userHeight = window.screen.height;
   }
 
 }
