f06b7e3b327d520c7ba0dc05b6d7496b2022aa12
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-02-19 22:30:37 +0100

    Api for project

--- HomePageBackend/server.py
+++ HomePageBackend/server.py
@@ -6,12 +6,20 @@ def getSkillInfo():
     with open("skills.json", "r") as skillFile:
         return json.load(skillFile)
 
+def getProjectInfo():
+    with open("projects.json", "r") as projectFile:
+        return json.load(projectFile)
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/skills")
 def skillApi():
     return json.dumps(getSkillInfo())
+
+@app.route("/projects")
+def projectApi():
+    return json.dumps(getProjectInfo())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
