8e4004e163277e5f43b5034ae9b55ef8fca12564
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-03-10 18:59:23 +0100

    Changed others and highlights to maps instead of lists

--- HomePageFrontend/src/models/about-me.ts
+++ HomePageFrontend/src/models/about-me.ts
@@ -1,11 +1,17 @@
 
 
 export class AboutMeModel{
+
+    //Things needed in the Side Bar
     name: string;
     country: string;
     place: string;
     address: string;
     github: string;
-    others: string;
+    others: Map<string, string>;
+
+    //Things needed in the main container(Mainly Text)
+    text: string;
+    highLights: Map<string, number[]>
     
 }
