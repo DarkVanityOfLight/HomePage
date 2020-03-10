db45a0cfc1b779bb3fa8c293029fe49a7438b08b
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-03-10 15:42:36 +0100

    Created and Implemented the about-me service

--- HomePageFrontend/src/services/about-me/about-me.service.ts
+++ HomePageFrontend/src/services/about-me/about-me.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { AboutMeModel } from '../../models/about-me';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AboutMeService {
+  private aboutMeUrl: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.aboutMeUrl = "http://localhost:8000/about-me";
+  }
+
+  get ABOUT_ME(): Observable<AboutMeModel>{
+
+    return this.http.get<AboutMeModel>(this.aboutMeUrl);
+  }
 }
