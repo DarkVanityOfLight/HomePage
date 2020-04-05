194f70afdd822e4e688f2756c33adfd7559ddc2f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-04-05 17:53:45 +0200

    Moced skill component
    Moved the skill component to start/skill

--- HomePageFrontend/src/bundels/sites/sites.module.ts
+++ HomePageFrontend/src/bundels/sites/sites.module.ts
@@ -5,7 +5,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { StartComponent } from './start/start.component';
 import { QuoteComponent } from './quote/quote.component';
-import { SkillComponent } from './skill/skill.component';
+import { SkillComponent } from './start/skill/skill.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProjectComponent } from './start/project/project.component'
 import { CommonThingsModule } from '../common/common-things.module';
